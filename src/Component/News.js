a9e39ef470ff84bd1daf070a8a71ee5eb8575322
Aarushi Arora <78602813+Aarushii12@users.noreply.github.com>
2024-08-12 18:41:01 +0530

    adding spinner

--- src/Component/News.js
+++ src/Component/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Newsitem from './Newsitem'
+import Spinner from './Spinner'
 
 
 export class News extends Component {
@@ -16,25 +17,32 @@ export class News extends Component {
   }
 
   async componentDidMount() {
-    let url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=7ca5aa28a57f40e29826367174fa0c6f"
+    let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=7ca5aa28a57f40e29826367174fa0c6f&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true })
     let data = await fetch(url);
+
     let parseddata = await data.json();
-    this.setState({ articles: parseddata.articles })
+    this.setState({ articles: parseddata.articles, totalResults: parseddata.totalResults, loading: false })
   }
 
   handlenext = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=7ca5aa28a57f40e29826367174fa0c6f&page=${this.state.page + 1}`;
-    let data = await fetch(url);
-    let parseddata = await data.json()
-    this.setState({
-      page: this.state.page + 1,
-      articles: parseddata.articles
-    })
 
+    if (!(this.state.page + 1 > Math.ceil(this.props.totalResults / this.props.pageSize))) {
+      let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=7ca5aa28a57f40e29826367174fa0c6f&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      let data = await fetch(url);
+      this.setState({ loading: true });
+      let parseddata = await data.json()
+      this.setState({
+        page: this.state.page + 1,
+        articles: parseddata.articles,
+        loading: false
+      })
+    }
   }
 
+
   handleprev = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=7ca5aa28a57f40e29826367174fa0c6f&page=${this.state.page - 1}`;
+    let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=7ca5aa28a57f40e29826367174fa0c6f&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parseddata = await data.json()
     this.setState({
@@ -47,13 +55,15 @@ export class News extends Component {
   render() {
     return (
       <div className="container my-3">
+        {this.state.loading && <Spinner />}
+
         <div className="row">
 
           {this.state.articles.map((element) => {
             return <div className="col-md-4" key={element.url} >
- 
+
               <Newsitem title={element.title ? element.title : ""} description={element.description ? element.description.slice(0, 80) : ""} imgurl={element.urlToImage} newsurl={element.url} />
-              
+
               <br></br>
             </div>
 
@@ -62,7 +72,7 @@ export class News extends Component {
 
             <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handleprev}>Previous</button>
 
-            <button type="button" className="btn btn-dark" onClick={this.handlenext}>Next</button>
+            <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handlenext}>Next</button>
 
           </div>
 
---
+++ src/Component/Spinner.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Load from './Load.gif'
+
+export class Spinner extends Component {
+  render() {
+    return (
+      <div className="text-center">
+        <img src={Load} alt="loading" />
+
+
+      </div>
+    )
+  }
+}
+
+export default Spinner
